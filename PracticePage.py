f1547df5689fa5b04a539ac55caed6d448c9df63
candi955 <44098788+candi955@users.noreply.github.com>
2020-02-08 13:48:15 -0700

    Attempting to sort linked lists.

--- PracticePage.py
+++ PracticePage.py
@@ -26,23 +26,332 @@ startGreen = '\033[92m'
 startYellow = '\033[93m'
 startRed = '\033[91m'
 
-randomNums100 = np.array(random.sample(range(1, 101), 100))
-print('\nRandom numbers:\n', randomNums100)
-randomNumsAsList = randomNums100.tolist()
-print('\nRandom numbers as a list:\n', randomNumsAsList)
 
-lst = dllist([randomNums100])
-print('\nDoubly linked list:\n', lst)
+# Starting by creating the doubly linked list classes and functions (the Node() and DoublyLinkedList() classes from
+# previous projects
+# Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+Node(data=None)
 
-listedAgain = lst.nodeat(0)
-print(startBlue + '\nlistedAgain:' + endColor, listedAgain)
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
 
-sortObj = pysort.Sorting()
-mergeSort100 = sortObj.mergeSort(randomNums100)
+    # inserting data into an empty list (Insert at end of list)
+    def insert_in_emptylist(self, data):
+            if self.start_node is None:
+                new_node = Node(data)
+                self.start_node = new_node
+            else:
+                print("list is not empty")
 
-timSort100 = sorted(randomNums100, reverse=False)
-print(startPurple + startBold + '\nTimsort 100 numbers:\n' + endColor, timSort100)
 
-print('\nUsing the Pysort package to ' + startPurple + startBold + 'Merge Sort 100 numbers ' +
-      endColor + 'the newarray100 list:\n\n', mergeSort100)
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
 
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+
+
+    def insert_after_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.pref = n
+                new_node.nref = n.nref
+                if n.nref is not None:
+                    n.nref.prev = new_node
+                n.nref = new_node
+
+    def insert_before_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.nref = n
+                new_node.pref = n.pref
+                if n.pref is not None:
+                    n.pref.nref = new_node
+                n.pref = new_node
+
+
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item, " ")
+                n = n.nref
+
+    def delete_at_start(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        self.start_node = self.start_node.nref
+        self.start_prev = None;
+
+     # deleting elements at the end of the list
+    def delete_at_end(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        n.pref.nref = None
+
+    # deleting elements by value
+    def delete_element_by_value(self, x):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                self.start_node = None
+            else:
+                print("Item not found")
+            return
+
+        if self.start_node.item == x:
+            self.start_node = self.start_node.nref
+            self.start_node.pref = None
+            return
+
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                break;
+            n = n.nref
+        if n.nref is not None:
+            n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                n.pref.nref = None
+            else:
+                print("Element not found")
+
+    # Reversing a doubly linked list
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        p = self.start_node
+        q = p.nref
+        p.nref = None
+        p.pref = q
+        while q is not None:
+            q.pref = q.nref
+            q.nref = p
+            p = q
+            q = q.pref
+        self.start_node = p
+
+    # Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+
+    # Attempting to create a function for 'find'
+    def find_element_by_value(self, x):
+
+        # for checking first node and finding the beginning number
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                print('Item found.')
+
+            else:
+                print("Item not found")
+            return
+
+        if self.start_node.item == x:
+            print("The number", x, "is the first number located on the list.")
+            self.start_node = self.start_node
+            self.start_node.pref = None
+            return
+        # for finding the middle numbers
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                print("The number", x, "is one of the numbers located in the middle of the list.")
+                break
+            n = n.nref
+
+        # for finding the end number
+        if n.nref is not None:
+            #n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                print("The number", x, "is the last number on the list.")
+            else:
+                print("Element not found")
+
+    def delete_AllElementsOfList(self, object):
+
+        for i in object:
+            if i + 1:
+                while True:
+                    try:
+
+
+                        if self.start_node is None:
+                            print("The list has no element to delete")
+                            return
+                        if self.start_node.nref is None:
+                            self.start_node = None
+                            return
+                        self.start_node = self.start_node.nref
+                        self.start_prev = None;
+
+                    except ValueError:
+                        print('There has been an error')
+                    else:
+                        break
+
+DoublyLinkedList()
+
+
+# Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+
+class Main():
+
+    randomNums100 = np.array(random.sample(range(1, 101), 100))
+    print('\nRandom numbers:\n', randomNums100)
+    randomNumsAsList = randomNums100.tolist()
+    print('\nRandom numbers as a list:\n', randomNumsAsList)
+
+    lst = dllist([randomNums100])
+    print('\nDoubly linked list:\n', lst)
+
+    listedAgain = lst.nodeat(0)
+    print(startBlue + '\nlistedAgain:' + endColor, listedAgain)
+
+    sortObj = pysort.Sorting()
+    mergeSort100 = sortObj.mergeSort(randomNums100)
+
+    timSort100 = sorted(randomNums100, reverse=False)
+    print(startPurple + startBold + '\nTimsort 100 numbers:\n' + endColor, timSort100)
+
+    print('\nUsing the Pysort package to ' + startPurple + startBold + 'Merge Sort 100 numbers ' +
+          endColor + 'the newarray100 list:\n\n', mergeSort100)
+
+    linked = DoublyLinkedList()
+    linked.insert_in_emptylist(randomNums100)
+    linked.traverse_list()
+    print('\nhello\n')
+    linked.traverse_list()
+    print('\nhelloagain\n')
+    print(np.array(listedAgain))
+    print('what')
+    updatedMergeList = np.array(listedAgain).tolist()
+    print(np.array(listedAgain).tolist())
+    print('why')
+    print(randomNums100)
+
+    print('whhhhhhaaaaaattttt')
+
+    ranNum100 = np.array(random.sample(range(1, 101), 100))
+    print('\nRandom numbers:\n', ranNum100)
+
+    ranNum100AsList = randomNums100.tolist()
+    print('\nRandom numbers as a list:\n', ranNum100AsList)
+
+    dblLinkedList = dllist([ranNum100])
+    print('\nDoubly linked list:\n', dblLinkedList)
+
+    dbllistedAgain = dblLinkedList.nodeat(0)
+    print(startBlue + '\nlistedAgain:' + endColor, dbllistedAgain)
+
+    print('\nwho\n')
+
+    dblListedAgainArray = np.array(dbllistedAgain)
+    print('where that at?')
+    print('\nDbl listed as array: ', dblListedAgainArray)
+    print('\nDbl listed again array as list(): ', dblListedAgainArray.tolist())
+    n = dblListedAgainArray.tolist()
+    l = len(dblLinkedList)
+    arandom = np.array(random.sample(range(1, 101), 100))
+    a = len(arandom)
+    aStr = str(arandom)
+
+    print('\nLength of Dbl length list list(): ', l)
+    print('\nLength of array(): ', a)
+    print('\nRegular array as string:', a)
+    print('\nwhat happened here?')
+
+    sep = ','
+
+    splitted = aStr.split()
+    print("\nHere is the regular array split by ',': ", splitted)
+    Strlinked = str(dblLinkedList)
+    splittedLinked = Strlinked.split()
+    print("\nLinked list as string, splitted: ", splittedLinked)
+
+
+    # iloc
+    #dblListedAgainIloc = dblListedAgainArray.iloc[:, 0]
+    #print('dblListedAgain Iloc: ', len(dblListedAgainIloc))
+
+    merging = sortObj.mergeSort(splittedLinked)
+    print('\nUsing the Pysort package to ' + startPurple + startBold + 'Merge Sort 100 numbers ' +
+          endColor + 'the string dbl linked list:\n\n', merging)
+    mergingAgain = sortObj.mergeSort(merging)
+    mergingAgainArray = np.array(mergingAgain)
+    mergingAgainArrayList = list(mergingAgainArray)
+    print('\nUsing the Pysort package to ' + startPurple + startBold + 'Merge Sort 100 numbers ' +
+          endColor + 'again in the dbl linked list:\n\n', mergingAgainArrayList)
+
+    print("\nColumn")
+
+
+
+Main()
